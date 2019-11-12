17057ad34671c8be06997260654dc67b62dc7dfb
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-11-11 16:49:01 -0800

    Add customizer to _.merge

--- src/utilities/merge.ts
+++ src/utilities/merge.ts
@@ -1,6 +1,10 @@
 import cloneDeep from "lodash/cloneDeep";
-import origMerge from "lodash/merge";
+import mergeWith from "lodash/mergeWith";
 
 export function merge<T, J>(object: T, source: J): T & J {
-  return origMerge<T, J>(cloneDeep(object), source);
+  return mergeWith<T, J>(cloneDeep(object), source, (leftVal, rightVal) => {
+    if (Array.isArray(leftVal) && Array.isArray(rightVal)) {
+      return rightVal;
+    }
+  });
 }
