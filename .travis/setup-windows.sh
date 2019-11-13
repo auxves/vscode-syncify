2e1f203b725596ccaa9ba5dd905bab337ec28485
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-11-13 15:34:54 -0800

    Use stages

--- .travis/setup-windows.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "travis@travis-ci.com"
-git config --global user.name "Travis"
--- .travis/should-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-[ $TRAVIS_NODE_VERSION = node ] && [ $TRAVIS_OS_NAME = linux ] && echo yes
