8fcfdb5683cf95f3a14fb79d8d5dd70dfedd381b
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2020-02-14 22:31:27 -0800

    Improve production builds

--- .webpack/production.js
+++ .webpack/production.js
@@ -2,8 +2,5 @@ const common = require("./common");
 const merge = require("lodash/merge");
 
 module.exports = merge(common, {
-  mode: "development",
-  optimization: {
-    minimize: true
-  }
+  mode: "production"
 });
